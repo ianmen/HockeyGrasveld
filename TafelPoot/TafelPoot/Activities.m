f853e2a269333e240a22a4c57cf359d357937eb5
StonJohnsson <info@stanjanssen.com>
2012-11-07 12:40:29 +0100

    -

---
+++ TafelPoot/TafelPoot/Activities.m
@@ -0,0 +1,13 @@
+//
+//  Activities.m
+//  TafelPoot
+//
+//  Created by Jeffrey on 10/23/12.
+//  Copyright (c) 2012 Avans Hogeschool. All rights reserved.
+//
+
+#import "Activities.h"
+
+@implementation Activities
+
+@end
