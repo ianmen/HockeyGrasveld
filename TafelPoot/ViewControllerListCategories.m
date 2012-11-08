7ab030e70454409902c62db765f81b7b0c337331
Jochem Rommens <Jochem@54-116.wifi-hsl.workstations.avans.nl>
2012-11-08 12:46:23 +0100

    Lijst ophalen GUI

---
+++ TafelPoot/ViewControllerListCategories.m
@@ -0,0 +1,67 @@
+//
+//  ViewControllerListCategories.m
+//  TafelPoot
+//
+//  Created by Jochem Rommens on 07-11-12.
+//  Copyright (c) 2012 Avans Hogeschool. All rights reserved.
+//
+
+#import "ViewControllerListCategories.h"
+
+@interface ViewControllerListCategories ()
+
+@end
+
+@implementation ViewControllerListCategories
+
+@synthesize categoryTable;
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"categoryCell"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // configure your cell here...
+    
+    cell.categoryLabel = @"Muziek";
+    
+    return cell;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
